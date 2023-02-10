a0a496691cd128a5e756ad69083bc2c77bc42c08
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-10 17:30:09 -0600

    feat(useProducts): add filter functionally

--- src/screens/Home/hooks/useProducts.ts
+++ src/screens/Home/hooks/useProducts.ts
@@ -4,15 +4,18 @@ import {getPointsNumber} from '../adapters';
 import {TProducts} from '../../../models/product.model';
 
 export const useProducts = () => {
+  const [localProducts, setLocalProducts] = useState<TProducts[]>([]);
   const [products, setProducts] = useState<TProducts[]>([]);
   const [totalPoints, setTotalPoints] = useState('0');
   const [loading, setLoading] = useState(true);
+  const [showAll, setShowAll] = useState(false);
 
   const getAllProducts = async () => {
     try {
       const response = await getProducts<TProducts[]>();
       if (response) {
-        setProducts(response.data);
+        setLocalProducts(response.data);
+        setProducts(response.data.filter(product => !product.is_redemption));
         const points = getPointsNumber(response.data);
         setTotalPoints(points);
       }
@@ -23,13 +26,32 @@ export const useProducts = () => {
     }
   };
 
+  const showAllProducts = () => {
+    setProducts(localProducts);
+    setShowAll(true);
+  }
+
+  const showWinProducts = () => {
+    setProducts(localProducts.filter(product => !product.is_redemption));
+    setShowAll(false);
+  }
+
+  const showRedemptionProducts = () => {
+    setProducts(localProducts.filter(product => product.is_redemption));
+    setShowAll(false);
+  }
+
   useEffect(() => {
     void getAllProducts();
   }, []);
 
   return {
     loading,
+    showAll,
     products,
     totalPoints,
+    showAllProducts,
+    showWinProducts,
+    showRedemptionProducts
   };
 };
