ff2a8b566c7d7a54fba5a3ff3f19fc66f6e3c9a1
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-10 16:32:13 -0600

    feat: add ProductsContainer component

---
+++ src/screens/Home/components/ProductsContainer/ProductsContainer.styles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+import {COLORS} from '../../../../assets/colors';
+
+export default StyleSheet.create({
+  content: {
+    flex: 1,
+    borderRadius: 10,
+    backgroundColor: COLORS.WHITE,
+    marginTop: 20,
+  },
+  container: {
+    marginVertical: 19,
+    paddingHorizontal: 10,
+    paddingBottom: 30,
+  },
+});
