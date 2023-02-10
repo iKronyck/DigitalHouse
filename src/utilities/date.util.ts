9f28b6d1630147eadef376e0bf2bf1347e259c53
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-10 16:18:45 -0600

    feat(date): add format to product item

--- src/utilities/date.util.ts
+++ src/utilities/date.util.ts
@@ -14,3 +14,13 @@ const MONTHS = [
 ];
 
 export const getCurrentMonth = () => MONTHS[new Date().getMonth()];
+
+export const formatDate = (date: string) => {
+  if (date && !isNaN(new Date(date).getTime())) {
+    const newDate = new Date(date);
+    return `${newDate.getDay()} de ${MONTHS[
+      newDate.getMonth()
+    ].toLocaleLowerCase()}, ${newDate.getFullYear()}`;
+  }
+  return '';
+};
