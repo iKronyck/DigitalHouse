80f4bd7a5345e800fa3031b2777fc154c301675f
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-11 00:37:47 -0600

    feat(home): redirect to product detail screen

--- src/screens/Home/components/ProductItem/ProductItem.tsx
+++ src/screens/Home/components/ProductItem/ProductItem.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, Pressable} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {Typography} from '../../../../components';
 import {formatDate} from '../../../../utilities';
-import ArrowIcon from '../../../../assets/icons/arrow.png';
 import {COLORS} from '../../../../assets/colors';
-import {TProductItem} from './ProductItem.types';
+import {TProductItem, StackNavigation} from './ProductItem.types';
 import {formatQuantityWithoutDecimals} from '../../../../adapters';
+import {ERouterRoutes} from '../../../../routes/models/router.model';
 import styles from './ProductItem.styles';
+
+import ArrowIcon from '../../../../assets/icons/arrow.png';
 
 export const ProductItem = ({
   name,
@@ -14,31 +17,45 @@ export const ProductItem = ({
   date,
   points,
   isRedemption,
-}: TProductItem) => (
-  <View style={styles.container}>
-    <View>
-      <Image style={styles.productImage} source={{uri: image}} />
-    </View>
-    <View style={styles.productInformation}>
-      <Typography fontSize={14} lineHeight={19} fontWeight="800">
-        {name}
-      </Typography>
-      <Typography fontSize={12} lineHeight={16} fontWeight="400">
-        {formatDate(date)}
-      </Typography>
-    </View>
-    <View style={styles.productQuantityContent}>
-      <Typography
-        color={isRedemption ? COLORS.RED : COLORS.GREEN}
-        fontSize={14}
-        lineHeight={19}
-        fontWeight="800">
-        {isRedemption ? '-' : '+'}
-      </Typography>
-      <Typography fontSize={14} lineHeight={19} fontWeight="800">
-        {formatQuantityWithoutDecimals(points)}
-      </Typography>
-      <Image style={styles.arrowIcon} source={ArrowIcon} />
-    </View>
-  </View>
-);
+}: TProductItem) => {
+  const navigation = useNavigation<StackNavigation>();
+  const product = {
+    product: name,
+    createdAt: date,
+    image,
+    points,
+    isRedemption,
+  };
+  return (
+    <Pressable
+      style={styles.container}
+      onPress={() =>
+        navigation.navigate(ERouterRoutes.MOTION_DETAIL_SCREEN, {product})
+      }>
+      <View>
+        <Image style={styles.productImage} source={{uri: image}} />
+      </View>
+      <View style={styles.productInformation}>
+        <Typography fontSize={14} lineHeight={19} fontWeight="800">
+          {name}
+        </Typography>
+        <Typography fontSize={12} lineHeight={16} fontWeight="400">
+          {formatDate(date)}
+        </Typography>
+      </View>
+      <View style={styles.productQuantityContent}>
+        <Typography
+          color={isRedemption ? COLORS.RED : COLORS.GREEN}
+          fontSize={14}
+          lineHeight={19}
+          fontWeight="800">
+          {isRedemption ? '-' : '+'}
+        </Typography>
+        <Typography fontSize={14} lineHeight={19} fontWeight="800">
+          {formatQuantityWithoutDecimals(points)}
+        </Typography>
+        <Image style={styles.arrowIcon} source={ArrowIcon} />
+      </View>
+    </Pressable>
+  );
+};
