10d589a6abdecb1fd66c65973552372539e512ea
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-12 01:33:30 -0600

    refactor(product-detail): move header and avoid use hook to get navigation

--- src/screens/ProductDetail/ProductDetailScreen.tsx
+++ src/screens/ProductDetail/ProductDetailScreen.tsx
@@ -1,65 +1,68 @@
 import React from 'react';
 import {View, Image, SafeAreaView, ScrollView} from 'react-native';
-import {useNavigation, useRoute} from '@react-navigation/native';
 import {AllButton} from '../Home/components';
-import {RouteNavigation, StackNavigation} from './ProductDetailScreen.types';
+import {TProductDetailScreen} from './ProductDetailScreen.types';
 import {Typography} from '../../components';
 import {COLORS} from '../../assets/colors';
 import {formatDate} from '../../utilities';
 import {formatQuantityWithoutDecimals} from '../../adapters';
+import {Header} from './components';
 import styles from './ProductDetailScreen.styles';
 
-export const MotionDetailScreen = () => {
-  const {
-    params: {product},
-  } = useRoute<RouteNavigation>();
-  const navigation = useNavigation<StackNavigation>();
+export const ProductDetailScreen = ({
+  navigation,
+  route,
+}: TProductDetailScreen) => {
+  const {product} = route.params;
   const points = formatQuantityWithoutDecimals(product.points);
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.content}>
-        <View style={styles.productImageContainer}>
-          <Image style={styles.productImage} source={{uri: product.image}} />
-        </View>
-        <Typography
-          fontSize={14}
-          lineHeight={19}
-          fontWeight="800"
-          color={COLORS.GRAY}>
-          Detalles del producto:
-        </Typography>
-        <View style={styles.textContainer}>
-          <Typography
-            fontSize={16}
-            lineHeight={22}
-            fontWeight="800"
-            color={COLORS.BLACK}>
-            {`Comprado el ${formatDate(product.createdAt)}`}
-          </Typography>
-        </View>
-        <View style={styles.textContainer}>
+    <>
+      <Header title={product.product} />
+      <View style={styles.container}>
+        <ScrollView style={styles.content}>
+          <View style={styles.productImageContainer}>
+            <Image style={styles.productImage} source={{uri: product.image}} />
+          </View>
           <Typography
             fontSize={14}
             lineHeight={19}
             fontWeight="800"
             color={COLORS.GRAY}>
-            Con esta compra acumulaste:
+            Detalles del producto:
           </Typography>
+          <View style={styles.textContainer}>
+            <Typography
+              fontSize={16}
+              lineHeight={22}
+              fontWeight="800"
+              color={COLORS.BLACK}>
+              {`Comprado el ${formatDate(product.createdAt)}`}
+            </Typography>
+          </View>
+          <View style={styles.textContainer}>
+            <Typography
+              fontSize={14}
+              lineHeight={19}
+              fontWeight="800"
+              color={COLORS.GRAY}>
+              Con esta compra acumulaste:
+            </Typography>
+          </View>
+          <View style={styles.pointText}>
+            <Typography
+              fontSize={16}
+              lineHeight={22}
+              fontWeight="800"
+              color={COLORS.BLACK}>
+              {points.concat(points === '1' ? ' punto' : ' puntos')}
+            </Typography>
+          </View>
+        </ScrollView>
+        <View style={styles.buttonContainer}>
+          <AllButton onPress={() => navigation.goBack()} text="Aceptar" />
         </View>
-        <View style={styles.pointText}>
-          <Typography
-            fontSize={16}
-            lineHeight={22}
-            fontWeight="800"
-            color={COLORS.BLACK}>
-            {points.concat(points === '1' ? ' punto' : ' puntos')}
-          </Typography>
-        </View>
-      </ScrollView>
-      <View style={styles.buttonContainer}>
-        <AllButton onPress={() => navigation.goBack()} text="Aceptar" />
+        <SafeAreaView />
       </View>
-      <SafeAreaView />
-    </View>
+    </>
   );
 };
--- src/screens/ProductDetail/ProductDetailScreen.types.ts
+++ src/screens/ProductDetail/ProductDetailScreen.types.ts
@@ -1,13 +1,7 @@
-import {NavigationProp, RouteProp} from '@react-navigation/native';
 import {ERouterRoutes, TRouterStack} from '../../routes/models/router.model';
-import {TProduct} from '../../models/product.model';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-export type RouteNavigation = RouteProp<
-  {params: {product: TProduct}},
-  'params'
->;
-
-export type StackNavigation = NavigationProp<
+export type TProductDetailScreen = NativeStackScreenProps<
   TRouterStack,
   ERouterRoutes.MOTION_DETAIL_SCREEN
 >;
