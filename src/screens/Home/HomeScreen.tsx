bcd55b80c1a8bb36687872eab2b44af55191e7aa
Israel Alfaro <israel.devalfaro@gmail.com>
2023-02-12 15:13:50 -0600

    refactor: add ErrorBoundary component

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -4,7 +4,13 @@ import {useProducts} from './hooks';
 import {Typography} from '../../components';
 import {COLORS} from '../../assets/colors';
 import {getCurrentMonth} from '../../utilities';
-import {AllButton, PointsCard, ProductsContainer, Header} from './components';
+import {
+  AllButton,
+  PointsCard,
+  ProductsContainer,
+  Header,
+  ErrorFallback,
+} from './components';
 import {THomeScreen} from './HomeScreen.types';
 import {ERouterRoutes} from '../../routes/models/router.model';
 import {TProduct} from '../../models/product.model';
@@ -16,6 +22,8 @@ export const HomeScreen = ({navigation}: THomeScreen) => {
     totalPoints,
     showAll,
     loading,
+    error,
+    resetError,
     showAllProducts,
     showWinProducts,
     showRedemptionProducts,
@@ -29,52 +37,54 @@ export const HomeScreen = ({navigation}: THomeScreen) => {
     <>
       <Header />
       <View style={styles.container}>
-        {loading ? (
-          <ActivityIndicator
-            testID="home-screen-loading"
-            size="large"
-            color={COLORS.PRIMARY}
-          />
-        ) : (
-          <>
-            <Typography
-              color={COLORS.GRAY}
-              fontWeight="800"
-              fontSize={14}
-              lineHeight={19}
-              textTransform="uppercase">
-              Tus puntos
-            </Typography>
-            <PointsCard month={currentMonth} points={totalPoints} />
-            <Typography
-              color={COLORS.GRAY}
-              fontWeight="800"
-              fontSize={14}
-              lineHeight={19}
-              textTransform="uppercase">
-              Tus movimientos
-            </Typography>
-            <ProductsContainer
-              testID="products-container"
-              products={products}
-              goToProductDetail={goToProductDetail}
+        <ErrorFallback error={error} resetErrorBoundary={resetError}>
+          {loading ? (
+            <ActivityIndicator
+              testID="home-screen-loading"
+              size="large"
+              color={COLORS.PRIMARY}
             />
-            <View style={styles.buttonsContainer}>
-              {showAll ? (
-                <>
-                  <AllButton onPress={showWinProducts} text="Ganados" />
-                  <View style={styles.divider} />
-                  <AllButton
-                    onPress={showRedemptionProducts}
-                    text="Canjeados"
-                  />
-                </>
-              ) : (
-                <AllButton onPress={showAllProducts} text="Todos" />
-              )}
-            </View>
-          </>
-        )}
+          ) : (
+            <>
+              <Typography
+                color={COLORS.GRAY}
+                fontWeight="800"
+                fontSize={14}
+                lineHeight={19}
+                textTransform="uppercase">
+                Tus puntos
+              </Typography>
+              <PointsCard month={currentMonth} points={totalPoints} />
+              <Typography
+                color={COLORS.GRAY}
+                fontWeight="800"
+                fontSize={14}
+                lineHeight={19}
+                textTransform="uppercase">
+                Tus movimientos
+              </Typography>
+              <ProductsContainer
+                testID="products-container"
+                products={products}
+                goToProductDetail={goToProductDetail}
+              />
+              <View style={styles.buttonsContainer}>
+                {showAll ? (
+                  <>
+                    <AllButton onPress={showWinProducts} text="Ganados" />
+                    <View style={styles.divider} />
+                    <AllButton
+                      onPress={showRedemptionProducts}
+                      text="Canjeados"
+                    />
+                  </>
+                ) : (
+                  <AllButton onPress={showAllProducts} text="Todos" />
+                )}
+              </View>
+            </>
+          )}
+        </ErrorFallback>
       </View>
     </>
   );
